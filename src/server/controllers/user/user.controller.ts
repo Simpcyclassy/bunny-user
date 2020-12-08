ecb28ae7c108c31dd080665ab2b045bd34b4cc3a
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-12-08 21:46:06 +0100

    added update user by id

--- src/server/controllers/user/user.controller.ts
+++ src/server/controllers/user/user.controller.ts
@@ -5,6 +5,8 @@ import {
   request,
   response,
   httpGet,
+  requestParam,
+  httpPatch
 } from "inversify-express-utils";
 import { Request, Response } from "express";
 import dotenv from "dotenv";
@@ -13,7 +15,7 @@ import { UserDTO, User } from "@app/data/user";
 import { BaseController } from "@app/data/utils";
 import { UserService } from "@app/services/authentication";
 import { UserRepo } from "@app/data/user";
-import { isUser } from "./user.validator";
+import { isUser, isID, isUserUpdate } from "./user.validator";
 import { secure } from "@app/data/utils";
 
 
@@ -37,6 +39,21 @@ export class UserController extends BaseController<ControllerResponse> {
   @httpGet("/")
   async getAllUsers(@request() req: Request, @response() res: Response) {
     const user = await UserRepo.all({});
+    this.handleSuccess(req, res, user);
+  }
+
+  @httpPatch("/:id", secure(isID), secure(isUserUpdate))
+  async updateUser(
+    @request() req: Request,
+    @response() res: Response,
+    @requestParam("id") id: string,
+    @requestBody() body: UserDTO
+  ) {
+    const update: any = {
+      name: body.name,
+    };
+
+    const user = await UserRepo.atomicUpdate(id, update);
     this.handleSuccess(req, res, user);
   }
 
